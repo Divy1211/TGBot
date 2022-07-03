f047bfd6c8f1da56dfed77af376d0aa8ad1c2502
Alian713 <divy1211.dc@gmail.com>
2022-07-03 16:49:29 +0800

    fixed nodemon typo

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -6,7 +6,7 @@
  *
  * @author Karol Majewski @ https://stackoverflow.com/a/54738437/7230293
  */
-export function ensure<T>(argument: T | undefined | null, message = "This value was promised to be there."): T {
+export function  ensure<T>(argument: T | undefined | null, message = "This value was promised to be there."): T {
     if (argument === undefined || argument === null) {
         throw new TypeError(message);
     }
