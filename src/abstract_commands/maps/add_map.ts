d8c395bfa18ee92b792c39354d7121dc40002be6
Siqi <siqxiang@gmail.com>
2022-07-17 15:21:01 +0800

    updated add_m command

---
+++ src/abstract_commands/maps/add_map.ts
@@ -0,0 +1,50 @@
+import {Guild} from "../../entities/Guild";
+import {GameMap} from "../../entities/pools/GameMap";
+import { Pool } from "../../entities/pools/Pool";
+import { PoolMap } from "../../entities/pools/PoolMap";
+
+/**
+ * Create a map with given name and imgLink, in the specified sever
+ * 
+ * @param map_name The name of the map
+ * @param pool_name The name of the pool
+ * @param multiplier The number of players for this map
+ * @param guildId The ID of the server in which the Map should be added
+ */
+export async function addMap(map_name: string, pool_name: string, multiplier: number, guildId: string) {
+    let guild = await Guild.findOneBy({id: guildId});
+    if (!guild) {
+        guild = new Guild(guildId);
+    }
+
+    var pool = await Pool.findOneBy({
+        name: pool_name
+    });
+
+    var map = await GameMap.findOneBy({
+        name: map_name
+    });
+
+    if (!pool){
+        return `Pool ${pool_name} is not found in this channel.`;
+    }
+    else if(!map){
+        return `Map ${map_name} is not found in this channel.`;
+    }
+
+    // else: check if map is already in the pool
+    let pool_map = await PoolMap.find({
+        where: {pool: {uuid: pool.uuid}}
+    });
+    
+    for (let i=0; i<pool_map.length; i++){
+        if (pool_map[i].map.name==map_name){
+            return `Invalid Operation: Map ${map_name} has already been in pool ${pool_name}!`;
+        }
+    }
+
+    // else: add the map into the pool
+    const target_map = new PoolMap(map, pool, multiplier);
+    await target_map.save();
+    return `Map ${map_name} is successfully added into Pool ${pool_name}!`
+}
--- src/commands/maps/add_m.ts
+++ src/commands/maps/add_m.ts
@@ -1,41 +1,49 @@
 import {ApplicationCommandOptionTypes} from "discord.js/typings/enums";
 import {ICommand} from "wokcommands";
-import { removeFromPool } from "../../abstract_commands/maps/remove_from_pool";
+import { addMap } from "../../abstract_commands/maps/add_map";
 import {ensure} from "../../utils/general";
 
 export default {
     category: "Admin",
-    description: "Remove a map from a pool",
+    description: "Add a map into a pool",
 
     slash: true,
     testOnly: true,
-    guildOnly: true,
 
     options: [
         {
-            name: "pool_name",
-            description: "the name of the pool",
+            name: "map_name",
+            description: "The name of the map",
             type: ApplicationCommandOptionTypes.STRING,
             required: true,
         },
         {
-            name: "map_name",
-            description: "the name of the map",
+            name: "pool_name",
+            description: "The name of the pool",
             type: ApplicationCommandOptionTypes.STRING,
+            required: true,
+        },
+        {
+            name: "multiplier",
+            description: "The number of players for this map",
+            type: ApplicationCommandOptionTypes.INTEGER,
             required: true,
         },
     ],
 
     callback: async ({interaction}) => {
         const {options, channelId, guildId} = interaction;
+
         // ensure that the command is being run in a server
         if (!channelId || !guildId) {
             return "This command can only be run in a text channel in a server";
         }
+
         // get the command parameters
-        const pool_name = ensure(options.getString("pool_name"));
         const map_name = ensure(options.getString("map_name"));
-        
-        return await removeFromPool(pool_name, map_name, guildId);
+        const pool_name = ensure(options.getString("pool_name"));
+        const multiplier = ensure(options.getInteger("multiplier"));
+
+        return await addMap(map_name, pool_name, multiplier, guildId)
     },
 } as ICommand;
