036b6104787af3307f1a30feb6c41d9f45a1d0b9
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2022-07-18 12:35:55 +0800

    remove a ban if it expires when calling ban

--- src/abstract_commands/ban/ban.ts
+++ src/abstract_commands/ban/ban.ts
@@ -20,15 +20,20 @@ export async function banUser(userId: string, duration: string|null, reason: str
 
     const discordId = userId;
     let user = await User.findOneBy({discordId});
-    console.log(user);
+
     if (!user) {
         user = new User(discordId, {guilds: [ensure(guild)]});
         await user.save();
     }
+
     let ban = await Ban.findOneBy({user:{discordId}});
+    console.log(ban);
     if (ban){
-        if (ban.until>0){
-            return `Error: <@${userId}> has already been banned for <t:${Math.floor(ban.until/1000)}:R>`;
+        if (ban.until > 0 && ban.until < new Date().getTime()/1000){
+            await ban.remove();
+        }
+        else if (ban.until > 0){
+            return `Error: <@${userId}> has already been banned for <t:${ban.until}:R>`;
         }
         else{
             return `Error: <@${userId}> has already been banned permanently`;
@@ -37,56 +42,52 @@ export async function banUser(userId: string, duration: string|null, reason: str
     if (!duration){
         ban = new Ban(user,reason,-1,guild);
         await ban.save();
+        if (!ban.reason){
+            return `<@${userId}> has been banned permanently`;
+        }   
+        return `<@${userId}> has been banned for "${ban.reason}" permanently`;
     }
-    else {
-        let expireTime;
-        let curTime = new Date().getTime();
-        
-        const [_, hh, mm, ss] = duration.match(/^(\d+):(\d{2})(:\d{2})?$/) ?? ["-1", "-1", "-1", "-1"];
-        // ss will be undefined if not specified
 
-        if(hh == "-1") {
+    let expireTime;
+    let curTime = new Date().getTime();
+    
+    const [_, hh, mm, ss] = duration.match(/^(\d+):(\d{2})(:\d{2})?$/) ?? ["-1", "-1", "-1", "-1"];
+    // ss will be undefined if not specified
+
+    if(hh == "-1") {
+        return "Error: The format of the specified duration is invalid, please try again;"
+    }
+    if (!ss){
+        try {
+            const h = parseInt(hh);
+            const m = parseInt(mm);
+            expireTime = h*60*60 + m*60
+            ban = new Ban(user,reason,Math.floor((expireTime*1000+curTime)/1000),guild);
+            await ban.save();
+        }
+        catch (e){
             return "Error: The format of the specified duration is invalid, please try again;"
         }
-        if (!ss){
-            try {
-                const h = parseInt(hh);
-                const m = parseInt(mm);
-                expireTime = h*60*60 + m*60
-                ban = new Ban(user,reason,Math.floor((expireTime*1000+curTime)/1000),guild);
-                await ban.save();
-            }
-            catch (e){
-                return "Error: The format of the specified duration is invalid, please try again;"
-            }
-        }
-        else{
-            try{
-                const h = parseInt(hh);
-                const m = parseInt(mm);
-                const s = parseInt(ss.slice(1));
-                expireTime = h*60*60 + m*60 + s;
-                ban = new Ban(user,reason,Math.floor((expireTime*1000+curTime)/1000),guild);
-                await ban.save();
+    }
+    else{
+        try{
+            const h = parseInt(hh);
+            const m = parseInt(mm);
+            const s = parseInt(ss.slice(1));
+            expireTime = h*60*60 + m*60 + s;
+            ban = new Ban(user,reason,Math.floor((expireTime*1000+curTime)/1000),guild);
+            await ban.save();
 
-            }
-            catch (e){
-                console.log(e)
-                return "Error: The format of the specified duration is invalid, please try again;"
-            }
+        }
+        catch (e){
+            console.log(e)
+            return "Error: The format of the specified duration is invalid, please try again;"
         }
     }
+
 
     if (!ban.reason){
-        if (ban.until>0){
-            return `<@${userId}> has been banned for <t:${ban.until}:R>`;
-        }
-        return `<@${userId}> has been banned permanently`;
+        return `<@${userId}> has been banned for <t:${ban.until}:R>`;
     }   
-    if (ban.until>0){
-        return `<@${userId}> has been banned for "${ban.reason}" for <t:${ban.until}:R>`;
-    }
-    return `<@${userId}> has been banned for "${ban.reason}" permanently`;
-
-
+    return `<@${userId}> has been banned for "${ban.reason}" for <t:${ban.until}:R>`;
 }
