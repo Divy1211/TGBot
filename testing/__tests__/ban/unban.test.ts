b0ea33118fdb69e0466e6f415f37c9ef0191d6a0
Alian713 <divy1211.dc@gmail.com>
2022-07-21 00:18:37 +0800

    ban and unban

--- testing/__tests__/ban/unban.test.ts
+++ testing/__tests__/ban/unban.test.ts
@@ -1,13 +1,10 @@
+import {banUser} from "../../../src/abstract_commands/ban/ban";
 import {Guild} from "../../../src/entities/Guild";
 import {User} from "../../../src/entities/User";
-import {Ban} from "../../../src/entities/user_data/Ban";
 
 beforeAll(async () => {
     const guild = new Guild("guild-1");
     await guild.save();
-
-    const user = new User("discord-id-1");
-    await user.save();
 });
 
 afterAll(async () => {
@@ -19,15 +16,16 @@ describe("Not Already Banned", () => {
     // !! Once done with writing the test cases, just remove the "todo: "
     // DO NOT remove the full comment, as it its still useful for documentation
 
-    // todo: missing minutes (hh)
-    // todo: missing minutes with seconds (hh::ss)
+    // todo: not already banned
 });
 
-describe("Valid Duration", () => {
+describe("Already Banned", () => {
     // !! Once done with writing the test cases, just remove the "todo: "
     // DO NOT remove the full comment, as it its still useful for documentation
 
-    // todo no duration (perm ban)
-    // todo: hh:mm
-    // todo: h:mm:ss
+    beforeEach(async () => {
+        await banUser("discord-id-1", "", "", "guild-id-1");
+    });
+
+    // todo: already banned
 });
