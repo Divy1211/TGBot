27321eba82f3067a618c486b1c08dca01cc0d751
imk8 <60835405+imk8@users.noreply.github.com>
2022-07-20 23:13:32 +0800

    Join with 1 UUID updated

--- testing/__tests__/queues/join.test.ts
+++ testing/__tests__/queues/join.test.ts
@@ -38,7 +38,15 @@ describe("Valid Join", () => {
         expect(queue.users[0].discordId).toBe("discord-id-1");
     });
 
-    // todo: join when there is just one single queue w/ queue uuid
+    //join when there is just one single queue w/ queue uuid
+    test("Join with 1 UUID", async() =>{
+        expect(
+            await joinQueue("discord-id-1", "channel-2", "guild-1",2),
+        ).toBeInstanceOf(MessageEmbed);
+
+        const queue = ensure(await Queue.findOneBy({uuid: 2}));
+        expect(queue.users[0].discordId).toBe("discord-id-1");
+    });
     // todo: join when there are multiple queues w/o queue uuid but user has a queue default
     // todo: join when there are multiple queues w/ queue uuid
     // todo: join with bypass ban
