b70653d23526a0e237997a0c834da3e049291c23
Alian713 <divy1211.dc@gmail.com>
2022-07-21 15:18:58 +0800

    Implemented test cases for unban

--- testing/__tests__/ban/unban.test.ts
+++ testing/__tests__/ban/unban.test.ts
@@ -1,3 +1,4 @@
+import {unbanUser} from "../../../src/abstract_commands/ban/unban";
 import {Guild} from "../../../src/entities/Guild";
 import {User} from "../../../src/entities/User";
 import {Ban} from "../../../src/entities/user_data/Ban";
@@ -18,21 +19,41 @@ afterAll(async () => {
     await User.remove(await User.find());
 });
 
-describe("Not Already Banned", () => {
-    // !! Once done with writing the test cases, just remove the "todo: "
-    // DO NOT remove the full comment, as it its still useful for documentation
+describe("Invalid Unban", () => {
 
-    // todo: not already banned
+    // not banned
+    test("Not Already Banned", async () => {
+        const discordId = "discord-id-1";
+
+        expect(
+            await unbanUser(discordId, "guild-1"),
+        ).toBe(`Error: <@${discordId}> is not banned`);
+    });
+
+    // not banned in the server the command is run in
+    test("Banned But Different Guild", async () => {
+        const ban = new Ban(user, "", -1, guild);
+        await ban.save();
+
+        const discordId = "discord-id-1";
+
+        expect(
+            await unbanUser(discordId, "guild-2"),
+        ).toBe(`Error: <@${discordId}> is not banned`);
+    });
 });
 
-describe("Already Banned", () => {
-    // !! Once done with writing the test cases, just remove the "todo: "
-    // DO NOT remove the full comment, as it its still useful for documentation
-
+describe("Valid Unban", () => {
     beforeEach(async () => {
         const ban = new Ban(user, "", -1, guild);
         await ban.save();
     });
 
-    // todo: already banned
+    test("Already Banned", async () => {
+        const discordId = "discord-id-1";
+
+        expect(
+            await unbanUser(discordId, "guild-1"),
+        ).toBe(`<@${discordId}> has been unbanned`);
+    });
 });
