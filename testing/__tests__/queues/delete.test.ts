35127fceeb30d3952523d88e6bfba6a5471f6c9a
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2022-07-21 23:50:50 +0800

    test delete

--- testing/__tests__/queues/delete.test.ts
+++ testing/__tests__/queues/delete.test.ts
@@ -1,8 +1,18 @@
 import {Guild} from "../../../src/entities/Guild";
 import {Leaderboard} from "../../../src/entities/queues/Leaderboard";
 import {Queue} from "../../../src/entities/queues/Queue";
+import {deleteQueue} from "../../../src/abstract_commands/queues/delete"
+import { ensure } from "../../../src/utils/general";
+import { Match } from "../../../src/entities/matches/Match";
+import { PlayerStats } from "../../../src/entities/queues/PlayerStats";
+import { User } from "../../../src/entities/User";
+import { PoolMap } from "../../../src/entities/pools/PoolMap";
+import { Pool } from "../../../src/entities/pools/Pool";
+
+
 
 let guild: Guild;
+let queue: Queue;
 
 beforeAll(async () => {
     guild = new Guild("guild-1");
@@ -17,14 +27,59 @@ describe("Invalid Delete", () => {
     // !! Once done with writing the test cases, just remove the "todo: "
     // DO NOT remove the full comment, as it its still useful for documentation
 
-    beforeAll(async () => {
-        const queue = new Queue("queue-1", guild, new Leaderboard(guild), 4, "channel-1");
+    beforeEach(async () => {
+        queue = new Queue("queue-1", guild, new Leaderboard(guild), 4, "channel-1");
         await queue.save();
     });
 
-    // todo: invalid uuid
-    // todo: uuid of queue in another channel (pass another channel to the delete function)
-    // todo: delete queue while match ongoing (create a match in the queue)
+    afterEach(async () => {
+        await Queue.remove(await Queue.find());
+    });
+
+    // invalid uuid
+    test("Invalid UUID",async () => {
+        expect(
+            await deleteQueue(10000000,"channel-1"),
+        ).toBe(`Error: Queue with ID \`10000000\` was not found in this channel.`)
+    })
+
+    // uuid of queue in another channel (pass another channel to the delete function)
+    test("Queue In Another Channel",async () => {
+        const queue2 = new Queue("queue-2", guild, new Leaderboard(guild), 4, "channel-2");
+        await queue2.save();
+        expect(
+            await deleteQueue(queue2.uuid, "channel-1")
+        ).toBe(`Error: Queue with ID \`${queue2.uuid}\` was not found in this channel.`)
+    })
+
+    // // todo: delete queue while match ongoing (create a match in the queue)
+    // test("Queue With Ongoing Match",async () => {
+    //     const queue = ensure(await Queue.findOne({
+    //         where: {name: "queue-1"},
+    //         relations: {
+    //             leaderboard: true,
+    //             pools: {poolMaps: true},
+    //             guild: true,
+    //         },
+    //     }));
+    //     const pool = new Pool("pool1",guild);
+    //     queue.pools = [pool]
+    //     await queue.save();
+
+    //     const user = new User("user1");
+    //     const playerStats = new PlayerStats(user,new Leaderboard(guild));
+    //     const match = new Match([playerStats],queue);
+    //     await match.save();
+    //     console.log(await Match.find());
+    //     const matches = await Match.findBy({
+    //         endTime: -1,
+    //         queue: {uuid: queue.uuid}
+    //     });
+    //     expect(
+    //         await deleteQueue(queue.uuid, "channel-1")
+    //     ).toBe(`Error: Cannot delete queue when a match is being played in the queue.`)
+    // })
+
 });
 
 describe("Valid Delete", () => {
@@ -32,9 +87,21 @@ describe("Valid Delete", () => {
     // DO NOT remove the full comment, as it its still useful for documentation
 
     beforeEach(async () => {
-        const queue = new Queue("queue-1", guild, new Leaderboard(guild), 4, "channel-1");
+        queue = new Queue("queue-1", guild, new Leaderboard(guild), 4, "channel-1");
         await queue.save();
     });
 
+    afterEach(async () => {
+        await Queue.remove(await Queue.find());
+    });
+
     // todo: correct uuid
+    test("Valid UUID",async () => {
+        expect(
+            await deleteQueue(queue.uuid,"channel-1")
+        ).toBe(`Queue "${queue.name}" with ID \`${queue.uuid}\` has been deleted successfully!`)
+        expect(
+            await Queue.findOneBy({name:"queue-1"})
+        ).toBeNull();
+    })
 });
