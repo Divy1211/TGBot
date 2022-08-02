af2a125b6344a93d45d5c3b35ab40567754417b0
Alian713 <divy1211.dc@gmail.com>
2022-08-02 17:49:06 +0800

    Implemented list bans command

---
+++ src/abstract_commands/ban/list_bans.ts
@@ -0,0 +1,48 @@
+import {MessageEmbed} from "discord.js";
+
+import {Ban} from "../../entities/user_data/Ban";
+import {ensure} from "../../utils/general";
+
+/**
+ * Returns a list of bans on the server
+ *
+ * @param guildId The ID of the server to list the bans for
+ */
+export async function listBans(guildId: string): Promise<MessageEmbed> {
+    const bans = await Ban.find({
+        where: {
+            guild: {id: guildId},
+        },
+        relations: {
+            user: true,
+        },
+    });
+
+    if (bans.length === 0) {
+        return new MessageEmbed()
+            .setTitle("Bans")
+            .setDescription("No bans")
+            .setColor("#ED2939");
+    }
+
+    return new MessageEmbed()
+        .setTitle("Bans")
+        .setColor("#ED2939")
+        .addFields([
+            {
+                name: "User",
+                value: `${bans.map(ban => `<@${ensure(ban.user).discordId}>`).join("\n")}`,
+                inline: true,
+            },
+            {
+                name: "Reason",
+                value: `${bans.map(ban => ban.reason || "-").join("\n")}`,
+                inline: true,
+            },
+            {
+                name: "Until",
+                value: `${bans.map(ban => ban.until !== -1 ? `<t:${ban.until}:R>` : "permanent").join("\n")}`,
+                inline: true,
+            },
+        ]);
+}
