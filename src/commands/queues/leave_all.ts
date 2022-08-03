58b5fff3c87e75dfef4442b8714794aa2bc8a1fa
Siqi <siqxiang@gmail.com>
2022-08-03 21:09:33 +0800

    updated leaveAll command

---
+++ src/commands/queues/leave_all.ts
@@ -0,0 +1,25 @@
+import {ICommand} from "wokcommands";
+import {leaveAll} from "../../abstract_commands/queues/leave_all";
+
+export default {
+    category: "User",
+    description: "Leave all queues",
+
+    slash: true,
+    testOnly: true,
+    guildOnly: true,
+
+    options: [],
+
+    callback: async ({interaction}) => {
+        const {channelId, guildId} = interaction;
+        
+        let {user} = interaction;
+
+        // ensure that the command is being run in a server
+        if (!channelId || !guildId)
+            return "This command can only be run in a text channel in a server";
+
+        return await leaveAll(user.id, guildId);
+    },
+} as ICommand;
