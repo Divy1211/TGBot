2eb2da38de93e4bc1e792d64030abd1071129af1
Alian713 <divy1211.dc@gmail.com>
2022-08-11 08:44:01 +0800

    fixed merge conflicts 2

--- src/commands/rating/rating_reset.ts
+++ src/commands/rating/rating_reset.ts
@@ -1,49 +1,31 @@
-import {MessageActionRow, MessageButton, MessageComponentInteraction} from "discord.js";
 import {ApplicationCommandOptionTypes} from "discord.js/typings/enums";
 import {ICommand} from "wokcommands";
-<<<<<<< HEAD
-import {Leaderboard} from "../../entities/queues/Leaderboard";
-import {User} from "../../entities/User";
-
-export default {
-    category: "General",
-    description: "This command resets the ratings of all the users in the server back to the default 1000.",
-=======
 import {reset} from "../../abstract_commands/ratings/reset";
 
 export default {
     category: "Admin",
     description: "Reset ratings for a user/queue",
->>>>>>> main
 
     slash: true,
     testOnly: true,
+    guildOnly: true,
 
     options: [
         {
             name: "user",
-<<<<<<< HEAD
-            description: "If specified, reset the ratings only for this user",
-=======
             description: "The user to reset ratings for. If unspecified, resets the ratings of all users",
->>>>>>> main
             type: ApplicationCommandOptionTypes.USER,
             required: false,
         },
         {
-<<<<<<< HEAD
-            name: "leaderboard_uuid",
-            description: "If specified, reset the ratings only for this leaderboard.",
-=======
             name: "queue_uuid",
             description: "The queue to reset the ratings for. If unspecified, resets the ratings in all queues",
->>>>>>> main
             type: ApplicationCommandOptionTypes.INTEGER,
             required: false,
         },
     ],
 
-    callback: async ({interaction, channel}) => {
+    callback: async ({interaction}) => {
         const {options, channelId, guildId} = interaction;
 
         // ensure that the command is being run in a server
@@ -52,107 +34,9 @@ export default {
         }
 
         // get the command parameters
-<<<<<<< HEAD
-        const userAPI = options.getUser("user") ?? null;
-        let user:any;
-        if (userAPI !== null){
-            user = await User.findOneBy({discordId:userAPI!.id})
-        }
-        const leaderboard_uuid = options.getInteger("leaderboard_uuid") ?? null;
-        let leaderboard: any;
-
-        if (leaderboard_uuid !== null) {
-            leaderboard = await Leaderboard.findOneBy({uuid: leaderboard_uuid});
-            if (leaderboard === null) {
-                return `Leaderboard with id ${leaderboard_uuid} does not exist`;
-            }
-        }
-
-        // create embedded message for users to confirm their action
-        const row = new MessageActionRow()
-            .setComponents(
-                new MessageButton()
-                    .setLabel('Cancel')
-                    .setStyle('SECONDARY')
-                    .setCustomId('cancel'),
-                new MessageButton()
-                    .setLabel('Confirm')
-                    .setStyle('DANGER')
-                    .setCustomId('confirm'),
-            )
-        const confirmMsg = await interaction.reply({
-            content: "Are you sure you want to reset rating values?",
-            components: [row],
-            fetchReply: true
-        })
-
-        const filter = (i: MessageComponentInteraction) => {
-            return i.user.id === interaction.user.id
-        };
-
-
-        let collector = channel.createMessageComponentCollector({filter, max: 1});
-
-        collector.on("collect", async (btnInt) => {
-            if (!btnInt) {
-                return;
-            }
-            await btnInt.deferUpdate();
-            if (btnInt.customId === "cancel") {
-                return;
-            } else if (btnInt.customId === "confirm") {
-                // leaderboard uuid is not provided
-                if (!leaderboard) {
-                    let leaderboards = await Leaderboard.find();
-                    // user is not provided
-                    if (!user) {
-                        for (let leaderboard of leaderboards) {
-                            let playerStats = leaderboard.playerStats;
-                            for (let playerStat of playerStats){
-                                playerStat.rating = 1000;
-                                await playerStat.save();
-                            }
-                        }
-                    }
-                    // otherwise
-                    else {
-                        for (let leaderboard of leaderboards) {
-                            let playerStats = leaderboard.playerStats;
-                                for (let playerStat of playerStats){
-                                    if (playerStat.user === user){
-                                        playerStat.rating = 1000;
-                                        await playerStat.save();
-                                    }
-                                }
-                        }
-                    }
-                } else {
-                    let playerStats = leaderboard.playerStats;
-                    if (!user){
-                        for (let playerStat of playerStats){
-                            playerStat.rating = 1000;
-                            await playerStat.save();
-                        }
-                    }
-                    else {
-                        for (let playerStat of playerStats){
-                            if (playerStat.user === user){
-                                playerStat.rating = 1000;
-                                await playerStat.save();
-                            }
-                        }
-                    }
-                }
-
-                await interaction.followUp(`The rating value has been set.`);
-            }
-        })
-    }
-=======
         const discordId = options.getUser("user")?.id;
         const queueUuid = options.getInteger("queue_uuid") ?? undefined;
 
         return await reset(guildId, discordId, queueUuid, {rating: true});
     },
->>>>>>> main
 } as ICommand;
--- src/commands/rating/rating_set.ts
+++ src/commands/rating/rating_set.ts
@@ -1,21 +1,16 @@
-import {MessageActionRow, MessageButton, MessageComponentInteraction} from "discord.js";
 import {ApplicationCommandOptionTypes} from "discord.js/typings/enums";
 import {ICommand} from "wokcommands";
-import {Leaderboard} from "../../entities/queues/Leaderboard";
-import {User} from "../../entities/User";
+import {ratingSet} from "../../abstract_commands/ratings/rating_set";
 import {ensure} from "../../utils/general";
 
+
 export default {
-<<<<<<< HEAD
-    category: "General",
-    description: "This command resets the ratings of all the users in the server back to the default 1000.",
-=======
     category: "Admin",
     description: "Set the rating for a user for a specific queue",
->>>>>>> main
 
     slash: true,
     testOnly: true,
+    guildOnly: true,
 
     options: [
         {
@@ -26,24 +21,19 @@ export default {
         },
         {
             name: "user",
-            description: "If specified, reset the ratings only for this user",
+            description: "The user to set rating for",
             type: ApplicationCommandOptionTypes.USER,
             required: true,
         },
         {
-<<<<<<< HEAD
-            name: "leaderboard_uuid",
-            description: "If specified, reset the ratings only for this leaderboard.",
-=======
             name: "queue_uuid",
             description: "The queue to set the rating for",
->>>>>>> main
             type: ApplicationCommandOptionTypes.INTEGER,
             required: false,
         },
     ],
 
-    callback: async ({interaction, channel}) => {
+    callback: async ({interaction}) => {
         const {options, channelId, guildId} = interaction;
 
         // ensure that the command is being run in a server
@@ -53,86 +43,9 @@ export default {
 
         // get the command parameters
         const rating = ensure(options.getInteger("rating"));
-<<<<<<< HEAD
-        const userAPI = ensure(options.getUser("user"));
-        const user = await User.findOneBy({discordId:userAPI!.id})
-
-        const leaderboard_uuid = options.getInteger("leaderboard_uuid") ?? null;
-        let leaderboard: any;
-
-        if (leaderboard_uuid !== null) {
-            leaderboard = await Leaderboard.findOneBy({uuid: leaderboard_uuid});
-            if (leaderboard === null) {
-                return `Leaderboard with id ${leaderboard_uuid} does not exist`;
-            }
-        }
-
-        // create embedded message for users to confirm their action
-        const row = new MessageActionRow()
-            .setComponents(
-                new MessageButton()
-                    .setLabel('Cancel')
-                    .setStyle('SECONDARY')
-                    .setCustomId('cancel'),
-                new MessageButton()
-                    .setLabel('Confirm')
-                    .setStyle('DANGER')
-                    .setCustomId('confirm'),
-            )
-        const confirmMsg = await interaction.reply({
-            content: `Are you sure you want to set rating values for ${userAPI.username}?`,
-            components: [row],
-            fetchReply: true
-        })
-
-        const filter = (i: MessageComponentInteraction) => {
-            return i.user.id === interaction.user.id
-        };
-
-
-        let collector = channel.createMessageComponentCollector({filter, max: 1});
-
-        collector.on("collect", async (btnInt) => {
-            if (!btnInt) {
-                return;
-            }
-            await btnInt.deferUpdate();
-            if (btnInt.customId === "cancel") {
-                return;
-            } else if (btnInt.customId === "confirm") {
-                // leaderboard uuid is not provided
-                if (!leaderboard) {
-                    let leaderboards = await Leaderboard.find();
-
-                    for (let leaderboard of leaderboards) {
-                        let playerStats = leaderboard.playerStats;
-                        for (let playerStat of playerStats){
-                            if (playerStat.user === user){
-                                playerStat.rating = rating;
-                                await playerStat.save();
-                            }
-                        }
-                    }
-                } else {
-                    let playerStats = leaderboard.playerStats;
-                    for (let playerStat of playerStats){
-                        if (playerStat.user === user){
-                            playerStat.rating = rating;
-                            await playerStat.save();
-                        }
-                    }
-
-                }
-
-                await interaction.followUp(`The rating value has been set.`);
-            }
-        })
-    }
-=======
         const discordId = ensure(options.getUser("user")?.id);
         const queueUuid = options.getInteger("queue_uuid") ?? undefined;
 
         return await ratingSet(discordId, rating, channelId, queueUuid);
     },
->>>>>>> main
 } as ICommand;
