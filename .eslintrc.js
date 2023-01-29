b6a3c77a4201739b46934ff550b6d9fdf3dc0161
bhavya <bhavyachandra2107@gmail.com>
2023-01-29 18:01:18 +0800

    [TGB-57] Add webstorm configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { off } = require("process");
+
 module.exports = {
   env: {
     browser: true,
@@ -11,7 +13,57 @@ module.exports = {
     sourceType: 'module',
     project: ['tsconfig.json'],
   },
+  plugins: [
+    "jsdoc",
+  ],
   rules: {
     'no-console': 'off',
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ["error", "never"],
+    "quotes": "off",
+    "import/newline-after-import": ["error", { "count": 2 }],
+    "@typescript-eslint/quotes": ["error", "double"],
+    "@typescript-eslint/semi": ["error", "always"],
+    "indent": 'off',
+    "@typescript-eslint/indent": ["error", 4],
+    "comma-dangle": 'off',
+    "@typescript-eslint/comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }],
+    "@typescript-eslint/consistent-type-imports": ["error", {
+      prefer: "no-type-imports",
+    }],
+    "consistent-type-assertions": "off",
+    "@typescript-eslint/consistent-type-assertions": ["error",
+      { 'assertionStyle': 'as', 'objectLiteralTypeAssertions': 'allow-as-parameter' }
+    ],
+    "strict-boolean-expressionss": "off",
+    "@typescript-eslint/strict-boolean-expressions": ["error", {
+      "allowString": false,
+      "allowNumber": false,
+      "allowNullableObject": true,
+      "allowNullableBoolean": true,
+      "allowNullableString": false,
+      "allowNullableNumber": false,
+      "allowAny": false
+    }],
+    "function-call-argument-newline": ["error", "consistent"],
+    "curly": ["error","all"],
+    "array-element-newline": ["error", "always"],
+    "jsdoc/no-types": 0,
+    "sort-imports": ["error", {
+      "ignoreCase": false,
+      "ignoreDeclarationSort": false,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+      "allowSeparatedGroups": false
+  }],
+  "newline-per-chained-call": ["error", { "ignoreChainWithDepth": 2 }],
+  "function-paren-newline": ["error", "never"],
+
   }
 }
